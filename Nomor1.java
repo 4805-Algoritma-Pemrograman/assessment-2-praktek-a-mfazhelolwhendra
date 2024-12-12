b46f98fc860d48229cbc63fdfdc895e722cab18e
mfazhelolwhendra <fazelybumble1001@gmail.com>
2024-12-12 09:08:59 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Nomor1 {
 
-   public static void angkaGanjil(int n) {
+    
+    public static void angkaGanjil(int n) {
         System.out.println("Angka ganjil dari 0 hingga " + n + " adalah:");
         for (int i = 1; i <= n; i += 2) {
             System.out.print(i + " ");
@@ -13,11 +14,14 @@ public class Nomor1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        
         System.out.print("Masukkan bilangan n: ");
         int n = scanner.nextInt();
 
+        
         angkaGanjil(n);
 
+        
         scanner.close();
     }
 }
